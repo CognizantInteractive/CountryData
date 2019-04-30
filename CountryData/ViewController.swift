32ce9ae82aa8bda72b6de745e074ad992bc978be
Mohammad Jahid <rajan.tiwari@cognizant.com>
2019-04-30 16:27:39 +0530

    Initial Commit

---
+++ CountryData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountryData
+//
+//  Created by CTS iMac Sierra on 30/04/19.
+//  Copyright © 2019 cognizant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
